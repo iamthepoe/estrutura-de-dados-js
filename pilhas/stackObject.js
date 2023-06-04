f462d2a1c3365e544bb3d20c3973889c9c420678
iamthepoe <lucagamerbrime@gmail.com>
2023-06-04 00:30:42 -0300

    add private fields

--- pilhas/stackObject.js
+++ pilhas/stackObject.js
@@ -1,45 +1,45 @@
 export class stackObject {
 	constructor() {
-		this.count = 0;
-		this.items = {};
+		this.#count = 0;
+		this.#items = {};
 	}
 
 	push(element) {
-		this.items[this.count] = element;
-		this.count++;
+		this.#items[this.#count] = element;
+		this.#count++;
 	}
 
 	pop() {
 		if (this.isEmpty()) return undefined;
-		const item = this.items[this.count];
-		delete this.items[this.count];
-		this.count--;
+		const item = this.#items[this.#count];
+		delete this.#items[this.#count];
+		this.#count--;
 		return item;
 	}
 
 	peek() {
 		if (this.isEmpty()) return undefined;
-		return this.items[this.count - 1];
+		return this.#items[this.#count - 1];
 	}
 
 	isEmpty() {
-		return this.count === 0;
+		return this.#count === 0;
 	}
 
 	size() {
-		return this.count;
+		return this.#count;
 	}
 
 	clear() {
-		this.items = {};
-		this.count = 0;
+		this.#items = {};
+		this.#count = 0;
 	}
 
 	toString() {
 		if (this.isEmpty()) return '';
-		let stringObj = `${this.items[0]}`;
-		for (let i = 1; i < this.count; i++) {
-			stringObj = `${stringObj}, ${this.items[i]}`;
+		let stringObj = `${this.#items[0]}`;
+		for (let i = 1; i < this.#count; i++) {
+			stringObj = `${stringObj}, ${this.#items[i]}`;
 		}
 		return stringObj;
 	}

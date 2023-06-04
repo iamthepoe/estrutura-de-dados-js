0977409ee86483ceb645989759dc9d476b9347f7
iamthepoe <lucagamerbrime@gmail.com>
2023-06-04 10:55:46 -0300

    push test

--- pilhas/__tests__/stackObject.test.js
+++ pilhas/__tests__/stackObject.test.js
@@ -7,4 +7,14 @@ describe('stackObject', () => {
 	beforeEach(() => {
 		stack = new StackObject();
 	});
+
+	it('should add a new element to the stack', () => {
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+
+		assert.equal(stack.peek(), 3);
+		assert.equal(stack.isEmpty(), false);
+		assert.equal(stack.size(), 3);
+	});
 });

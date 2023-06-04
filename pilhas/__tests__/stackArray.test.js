e67bdb6f442e6ecbc4fb147e7954a946f1dc046f
iamthepoe <lucagamerbrime@gmail.com>
2023-06-04 00:38:43 -0300

    first test

--- pilhas/__tests__/stackArray.test.js
+++ pilhas/__tests__/stackArray.test.js
@@ -1,2 +1,8 @@
-import { StackArray } from '../stackArray';
+import assert from 'node:assert';
+import { it } from 'node:test';
+import { StackArray } from '../stackArray.js';
 const stackArray = new StackArray();
+
+it('should be defined', () => {
+	assert.ok(new StackArray());
+});

a4641253fd948368b9121c1e607e212982d3411d
iamthepoe <lucagamerbrime@gmail.com>
2023-06-04 10:03:18 -0300

    refactoring

--- pilhas/__tests__/stackArray.test.js
+++ pilhas/__tests__/stackArray.test.js
@@ -1,16 +1,11 @@
 import assert from 'node:assert';
-import { it } from 'node:test';
+import { beforeEach, it } from 'node:test';
 import { StackArray } from '../stackArray.js';
 const stackArray = new StackArray();
 
-it('should be defined', () => {
-	assert.ok(stackArray);
-});
-
-it('should return true if is empty', () => {
-	assert.equal(stackArray.isEmpty(), true);
-});
-
-it('should return the stack size', () => {
-	assert.equal(stackArray.size(), 0);
+describe('stackArray', () => {
+	let stack;
+	beforeEach(() => {
+		stack = new StackArray();
+	});
 });

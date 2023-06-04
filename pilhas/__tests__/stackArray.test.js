d8a086c01cb397513a006ef77bd2afcbdfadadf6
iamthepoe <lucagamerbrime@gmail.com>
2023-06-04 10:14:09 -0300

    push test

--- pilhas/__tests__/stackArray.test.js
+++ pilhas/__tests__/stackArray.test.js
@@ -1,5 +1,5 @@
 import assert from 'node:assert';
-import { beforeEach, it } from 'node:test';
+import { describe, beforeEach, it } from 'node:test';
 import { StackArray } from '../stackArray.js';
 const stackArray = new StackArray();
 
@@ -7,5 +7,14 @@ describe('stackArray', () => {
 	let stack;
 	beforeEach(() => {
 		stack = new StackArray();
+	});
+
+	it('should add a new element to the stack', () => {
+		stack.push(1);
+		stack.push(2, 3);
+
+		assert.equal(stack.size(), 3);
+		assert.equal(stack.isEmpty(), false);
+		assert.equal(stack.peek(), 3);
 	});
 });

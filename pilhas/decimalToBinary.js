d851598dcb397ce0b0fb725afa2fcb1e32f548f9
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 09:36:03 -0300

    first implementation

--- pilhas/decimalToBinary.js
+++ pilhas/decimalToBinary.js
@@ -1,4 +1,19 @@
 import { StackObject } from './stackObject.js';
 const remStack = new StackObject();
 
-const decimalToBinary = (decimalNumber) => {};
+const decimalToBinary = (decimalNumber) => {
+	let rem;
+	let toStringBinary = '';
+	while (decimalNumber > 0) {
+		rem = Math.floor(decimalNumber % 2);
+		remStack.push(rem);
+		decimalNumber = Math.floor(decimalNumber / 2);
+	}
+	let size = remStack.size();
+	for (let i = 0; i < size; i++) {
+		toStringBinary += String(remStack.pop());
+	}
+	return toStringBinary;
+};
+
+console.log(decimalToBinary(10));

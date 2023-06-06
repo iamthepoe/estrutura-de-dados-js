c0ff15bbae5d205b42c9364fa0530be30c2d237a
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 09:47:05 -0300

    describe

--- pilhas/__tests__/decimalToBinary.test.js
+++ pilhas/__tests__/decimalToBinary.test.js
@@ -1,3 +1,9 @@
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { decimalToBinary } from '../decimalToBinary.js';
+
+describe('decimalToBinary', () => {
+	it('should return a string', () => {
+		decimalToBinary(10);
+	});
+});

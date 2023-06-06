6187f026c5f1bd862734846c0fb2ac22ac6c6949
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 10:18:47 -0300

    should be defined

--- pilhas/__tests__/decimalToBinary.test.js
+++ pilhas/__tests__/decimalToBinary.test.js
@@ -3,6 +3,10 @@ import { describe, it } from 'node:test';
 import { decimalToBinary } from '../decimalToBinary.js';
 
 describe('decimalToBinary', () => {
+	it('should be defined', () => {
+		assert.ok(decimalToBinary);
+	});
+
 	it('should return a string', () => {
 		assert.equal(typeof decimalToBinary(10), 'string');
 	});

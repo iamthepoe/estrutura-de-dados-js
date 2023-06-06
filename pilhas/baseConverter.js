64bef298708fdfe5c37090397ef86a6335859d4d
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 10:49:06 -0300

    variables

--- pilhas/baseConverter.js
+++ pilhas/baseConverter.js
@@ -3,4 +3,7 @@ import { StackObject } from './stackObject';
 const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const stack = new StackObject();
 
-const baseConverter = (decimalNumber, base) => {};
+const baseConverter = (decimalNumber, base) => {
+	let rem;
+	let baseString;
+};

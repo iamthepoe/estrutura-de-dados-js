ff8d118ca74d8540dc8925667536031c405c99f9
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 10:50:29 -0300

    add if for restrict bases

--- pilhas/baseConverter.js
+++ pilhas/baseConverter.js
@@ -1,9 +1,10 @@
-import { StackObject } from './stackObject';
+import { StackObject } from './stackObject.js';
 
 const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const stack = new StackObject();
 
 const baseConverter = (decimalNumber, base) => {
 	let rem;
-	let baseString;
+	let baseString = ' ';
+	if (!(base >= 2 && base <= 36)) return '';
 };

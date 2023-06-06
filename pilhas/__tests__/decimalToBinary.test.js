345323a4631207cff9e844ea97e50706043ad962
iamthepoe <lucagamerbrime@gmail.com>
2023-06-06 09:49:38 -0300

    convert 10 test

--- pilhas/__tests__/decimalToBinary.test.js
+++ pilhas/__tests__/decimalToBinary.test.js
@@ -6,4 +6,8 @@ describe('decimalToBinary', () => {
 	it('should return a string', () => {
 		assert.equal(typeof decimalToBinary(10), 'string');
 	});
+
+	it('should convert 10 to 1010', () => {
+		assert.equal(decimalToBinary(10), '1010');
+	});
 });

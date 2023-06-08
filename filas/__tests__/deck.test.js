e498cd7c523358a474bdc724ee8fc3f45647fdab
iamthepoe <lucagamerbrime@gmail.com>
2023-06-08 20:55:36 -0300

    beforeEach

--- filas/__tests__/deck.test.js
+++ filas/__tests__/deck.test.js
@@ -2,4 +2,9 @@ import assert from 'node:assert';
 import { beforeEach, describe, it } from 'node:test';
 import { Deck } from '../deck.js';
 
-describe('Deck', () => {});
+describe('Deck', () => {
+	let deck;
+	beforeEach(() => {
+		deck = new Deck();
+	});
+});

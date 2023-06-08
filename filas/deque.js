7ad687514d356f4af5c37275bdf369146c07d278
iamthepoe <lucagamerbrime@gmail.com>
2023-06-08 20:46:36 -0300

    addFront

--- filas/deque.js
+++ filas/deque.js
@@ -3,6 +3,22 @@ export class Deque {
 	#lowestCount = 0;
 	#items = {};
 
+	addFront(element) {
+		if (this.isEmpty()) return this.addBack(element);
+
+		if (this.#lowestCount > 0) {
+			this.#lowestCount--;
+			this.#items[this.#lowestCount] = element;
+		} else {
+			for (let i = this.#count; i > 0; i--) {
+				this.#items[i] = this.#items[i - 1];
+			}
+			this.#count++;
+			this.#lowestCount = 0;
+			this.#items[0] = element;
+		}
+	}
+
 	addBack(element) {
 		this.#items[this.#count] = element;
 		this.#count++;

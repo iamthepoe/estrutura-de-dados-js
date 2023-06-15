268d748596ddd724731c294188c21fa7075fd41f
iamthepoe <lucagamerbrime@gmail.com>
2023-06-15 20:39:10 -0300

    add forEach

--- filas/hotPotato.js
+++ filas/hotPotato.js
@@ -2,4 +2,9 @@ import { Queue } from './queue.js';
 
 const hotPotato = (elementList, num) => {
 	const queue = new Queue();
+	const eliminatedList = [];
+
+	elementList.forEach((element) => {
+		queue.enqueue(element);
+	});
 };

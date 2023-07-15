e06ac06be86946c3b1e70bf6482bdb782865a4b4
iamthepoe <luca1901p@gmail.com>
2023-07-15 19:30:28 -0300

    calling function

--- filas/hotPotato.js
+++ filas/hotPotato.js
@@ -1,22 +1,24 @@
 import { Queue } from './queue.js';
 
 export const hotPotato = (elementList, num) => {
-	const queue = new Queue();
-	const eliminated = [];
+  const queue = new Queue();
+  const eliminated = [];
 
-	elementList.forEach((element) => {
-		queue.enqueue(element);
-	});
+  elementList.forEach((element) => {
+    queue.enqueue(element);
+  });
 
-	while (queue.size() > 1) {
-		for (let i = 0; i < num; i++) {
-			queue.enqueue(queue.dequeue);
-		}
-		eliminated.push(queue.dequeue());
-	}
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+    eliminated.push(queue.dequeue());
+  }
 
-	return {
-		eliminated,
-		winner: queue.dequeue(),
-	};
+  return {
+    eliminated,
+    winner: queue.dequeue(),
+  };
 };
+
+console.log(hotPotato(['a', 'b'], 3));

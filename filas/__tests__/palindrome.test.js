e0c0535af376f905cdbad2d1ef4884db4b8b4e1c
iamthepoe <luca1901p@gmail.com>
2023-07-15 20:54:39 -0300

    write true tests

--- filas/__tests__/palindrome.test.js
+++ filas/__tests__/palindrome.test.js
@@ -1,12 +1,12 @@
 import assert from 'node:assert';
 import { beforeEach, describe, it } from 'node:test'
-import { palindromeChecker } from '../palindrome.js';
+import { isPalindrome } from '../palindrome.js';
 
 describe('palindrome', () => {
   it('should return true', () => {
-    assert.ok(palindromeChecker('arara'));
-    assert.ok(palindromeChecker('Subi no onibus'));
-    assert.ok(palindromeChecker('step on no pets'));
-    assert.ok(palindromeChecker('kayak'));
+    assert.ok(isPalindrome('arara'));
+    assert.ok(isPalindrome('Subi no onibus'));
+    assert.ok(isPalindrome('step on no pets'));
+    assert.ok(isPalindrome('kayak'));
   });
 });

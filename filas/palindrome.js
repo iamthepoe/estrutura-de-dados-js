f926e2512d7dcdf62271a8ca77c27bfcf5536909
iamthepoe <luca1901p@gmail.com>
2023-07-15 20:32:09 -0300

    palindrome algorithm

---
+++ filas/palindrome.js
@@ -0,0 +1,20 @@
+import { Deck } from "./deck.js";
+
+export const palindromeChecker = (aString) => {
+  if (!aString) return false;
+
+  const deck = new Deck();
+
+  const lowerString = aString.toLocaleLowerCase().trim();
+  let isEqual = true;
+  let firstChar, lastChar;
+  for (let i = 0; i < lowerString.length; i++) {
+    deck.addBack(lowerString.charAt(i));
+  }
+  while (deck.size() > 1 && isEqual) {
+    firstChar = deck.removeFront();
+    lastChar = deck.removeBack();
+    if (firstChar !== lastChar) isEqual = false;
+  }
+  return isEqual;
+}

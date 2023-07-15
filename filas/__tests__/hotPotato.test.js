50179d90ebe78364868b30739388e9c0e4c6ccf4
iamthepoe <luca1901p@gmail.com>
2023-07-15 19:33:22 -0300

    first test for hot potato

--- filas/__tests__/hotPotato.test.js
+++ filas/__tests__/hotPotato.test.js
@@ -1,3 +1,11 @@
 import assert from 'node:assert';
 import { beforeEach, describe, it } from 'node:test';
-import { hotPotato } from '../hotPotato';
+import { hotPotato } from '../hotPotato.js';
+
+describe('hotPotato', () => {
+  it('should John be the winner', () => {
+    const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
+    const result = hotPotato(names, 7);
+    assert.equal(result.winner, 'John');
+  });
+});

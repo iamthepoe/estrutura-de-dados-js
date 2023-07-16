85435c9fd8367935dc6dc12dafc8af9001e1e348
iamthepoe <luca1901p@gmail.com>
2023-07-15 23:01:47 -0300

    rename class

--- deque/deque.js
+++ deque/deque.js
@@ -1,76 +1,76 @@
-export class Deck {
-	#count = 0;
-	#lowestCount = 0;
-	#items = {};
+export class Deque {
+  #count = 0;
+  #lowestCount = 0;
+  #items = {};
 
-	addFront(element) {
-		if (this.isEmpty()) return this.addBack(element);
+  addFront(element) {
+    if (this.isEmpty()) return this.addBack(element);
 
-		if (this.#lowestCount > 0) {
-			this.#lowestCount--;
-			this.#items[this.#lowestCount] = element;
-		} else {
-			for (let i = this.#count; i > 0; i--) {
-				this.#items[i] = this.#items[i - 1];
-			}
-			this.#count++;
-			this.#lowestCount = 0;
-			this.#items[0] = element;
-		}
-	}
+    if (this.#lowestCount > 0) {
+      this.#lowestCount--;
+      this.#items[this.#lowestCount] = element;
+    } else {
+      for (let i = this.#count; i > 0; i--) {
+        this.#items[i] = this.#items[i - 1];
+      }
+      this.#count++;
+      this.#lowestCount = 0;
+      this.#items[0] = element;
+    }
+  }
 
-	addBack(element) {
-		this.#items[this.#count] = element;
-		this.#count++;
-	}
+  addBack(element) {
+    this.#items[this.#count] = element;
+    this.#count++;
+  }
 
-	removeFront() {
-		if (this.isEmpty()) return undefined;
+  removeFront() {
+    if (this.isEmpty()) return undefined;
 
-		const element = this.#items[this.#lowestCount];
-		delete this.#items[this.#lowestCount];
-		this.#lowestCount++;
-		return element;
-	}
+    const element = this.#items[this.#lowestCount];
+    delete this.#items[this.#lowestCount];
+    this.#lowestCount++;
+    return element;
+  }
 
-	removeBack() {
-		if (this.isEmpty()) return undefined;
-		const item = this.#items[this.#count - 1];
-		delete this.#items[this.#count - 1];
-		this.#count--;
-		return item;
-	}
+  removeBack() {
+    if (this.isEmpty()) return undefined;
+    const item = this.#items[this.#count - 1];
+    delete this.#items[this.#count - 1];
+    this.#count--;
+    return item;
+  }
 
-	peekFront() {
-		if (this.isEmpty()) return undefined;
-		return this.#items[this.#lowestCount];
-	}
+  peekFront() {
+    if (this.isEmpty()) return undefined;
+    return this.#items[this.#lowestCount];
+  }
 
-	peekBack() {
-		if (this.isEmpty()) return undefined;
-		return this.#items[this.#count - 1];
-	}
+  peekBack() {
+    if (this.isEmpty()) return undefined;
+    return this.#items[this.#count - 1];
+  }
 
-	size() {
-		return this.#count - this.#lowestCount;
-	}
+  size() {
+    return this.#count - this.#lowestCount;
+  }
 
-	isEmpty() {
-		return this.size() === 0;
-	}
+  isEmpty() {
+    return this.size() === 0;
+  }
 
-	clear() {
-		this.#count = 0;
-		this.#lowestCount = 0;
-		this.#items = {};
-	}
+  clear() {
+    this.#count = 0;
+    this.#lowestCount = 0;
+    this.#items = {};
+  }
 
-	toString() {
-		if (this.isEmpty()) return '';
-		let objString = `${this.#items[this.#lowestCount]}`;
-		for (let i = this.#lowestCount + 1; i < this.#count; i++) {
-			objString = `${objString},${this.#items[i]}`;
-		}
-		return objString;
-	}
+  toString() {
+    if (this.isEmpty()) return '';
+    let objString = `${this.#items[this.#lowestCount]}`;
+    for (let i = this.#lowestCount + 1; i < this.#count; i++) {
+      objString = `${objString},${this.#items[i]}`;
+    }
+    return objString;
+  }
 }
